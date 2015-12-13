ea22b6141c4490d1365dd698d60e54c0fe97a5d0
Henry Xiang <henryxiang@gmail.com>
2015-12-12 20:54:39 -0800

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+	entry: './main.js',
+	output: {
+		path: './',
+		filename: 'index.js'
+	},
+	devServer: {
+		inline: true,
+		port: 9000
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.jsx?$/,
+				exclude: /node_modules/,
+				loader: 'babel',
+				query: {presets: ['stage-0', 'es2015', 'react']}
+			}
+		]
+	}
+}
